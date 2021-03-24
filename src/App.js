0de2bedaa6cf5a2841aa5219f0517c50e68edb1b
neurotica <martin.n.villarreal@gmail.com>
2021-03-24 01:56:21 -0300

    tercera clase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+/*
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Antitodo E-Commerce</h1>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import './App.css'
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>Inicio</li>
+        <li>Noticias</li>
+        <li>e-commerce</li>
+        <li>Acerca de</li>
+      </ul>
     </div>
   );
 }
